e7c7f8cb4c6db214cf7760c96afc1e94afe55ddb
jainshanky11 <jainshanky11@gmail.com>
2019-01-10 23:05:36 +0530

    realtime project2

---
+++ create-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+container_count=$1
+  echo"================================================"
+  echo "Total $container_count will be created"
+sleep 2;
+for i in `seq $container_count`
+do
+  echo "creating container $i "
+sleep 1
+  docker run -it --name dockerfilecontainer$i jainshanky11/dockerfile-img4:tomcat /bin/bash
+  echo "container $i was created"
+  echo "=============================================="
+done
