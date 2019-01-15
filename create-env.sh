3c6315ed8ca94352adb0946c0a6f63cb9873bc21
jainshanky11 <jainshanky11@gmail.com>
2019-01-15 23:46:23 +0530

    modified create-env file

--- create-env.sh
+++ create-env.sh
@@ -7,7 +7,7 @@ for i in `seq $container_count`
 do
   echo "creating container $i "
 sleep 1
-  docker run -it --name dockerfilecontainer$i jainshanky11/dockerfile-img4:tomcat /bin/bash
+  docker run -it -d --name dockerfilecontainer$i jainshanky11/dockerfile-img4:tomcat /bin/bash
   echo "container $i was created"
   echo "=============================================="
 done
